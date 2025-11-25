64361b032f8b6543ac6967be3b1d2b318c1f4142
nedmamiala-prog <nedmamiala@gmail.com>
2025-11-25 21:58:39 +0800

    adasdas

--- back-end/server.js
+++ back-end/server.js
@@ -59,7 +59,8 @@ app.get('/test-db', (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
   startNotificationScheduler();
 });
